ea2ada9ebcefff83a960b655712ed8931526e491
JudeLawrence <judesmail@gmail.com>
2021-05-30 15:47:43 +0400

    Added destroy action to bookmarks. Passed all rake tests

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -20,8 +20,8 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:bookmark_id])
-    @bookmark.delete
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
   end
 
   private
